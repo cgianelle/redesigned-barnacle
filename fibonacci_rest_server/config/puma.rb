b6aa73a1b5c2093974659865affd06b13fa44915
Chris Gianelle <cgianelle@cipherhealth.com>
2021-11-05 17:46:00 -0400

    Added workers to config

--- fibonacci_rest_server/config/puma.rb
+++ fibonacci_rest_server/config/puma.rb
@@ -3,4 +3,5 @@ require('logger')
 require_relative('settings')
 
 port(Config::Settings::REDESIGNED_BARNACLE_PORT)
-threads(Config::Settings::REDESIGNED_BARNACLE_MIN_WORKERS, Config::Settings::REDESIGNED_BARNACLE_MAX_WORKERS)
+threads(Config::Settings::REDESIGNED_BARNACLE_MIN_THREADS, Config::Settings::REDESIGNED_BARNACLE_MAX_THREADS)
+workers(Config::Settings::REDESIGNED_BARNACLE_MAX_WORKERS)
